517b4630b83c7c17a903fa15f9d4abb12b70167d
guigas-cmd <diasnascimentoguilherme@gmail.com>
2025-11-26 00:34:56 -0300

    CurrencyConverter.java simplified

--- src/entities/CurrencyConverter.java
+++ src/entities/CurrencyConverter.java
@@ -4,9 +4,7 @@ public class CurrencyConverter {
     public static final double IOF = 0.06;
 
     public static double converter(double dollarPrice, double reaisPrice) {
-        double totalWithoutIOF = dollarPrice * reaisPrice;
-        double iofValue = totalWithoutIOF * IOF;
-        return totalWithoutIOF + iofValue;
+        return dollarPrice * reaisPrice * (1.0 + IOF);
 
     }
 }
